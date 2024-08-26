948f320325dc4655b8458dcf26f1e607ad8f1932
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-08-26 17:22:48 +0300

    Добавьте файлы проекта.

---
+++ EulerFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EulerFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedit 4islo");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Число Елера {EulerFunction(a)}");
+
+        }
+        static int EulerFunction(int number)
+        {
+            double temp = 1;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    temp *= ((Math.Pow(i, 1) - Math.Pow(i, 0)));
+                }
+                else if (number / i == 1)
+                {
+                    break;
+                }
+            }
+            return Convert.ToInt32(temp);
+        }
+    }
+}
